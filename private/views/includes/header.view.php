4e4b97be6b80a09fd7d1eaa5d06b8c24caa27833
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2023-09-27 13:28:44 +0530

    hover effects

--- private/views/includes/header.view.php
+++ private/views/includes/header.view.php
@@ -3,12 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Free Web tutorials">
-    <meta name="keywords" content="HTML, CSS,Javascript">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-
+    <title>Document</title>
+    <link rel="stylesheet" href=" <?= ASSETS ?>css/styles.css">
 </head>
-
-<body>
-    <div style="min-width: 350px;">
