05a804bdb7d3056cb3773fa8709e3b935352f982
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2023-09-28 21:49:17 +0530

    input and checkboxes added

--- private/views/includes/header.view.php
+++ private/views/includes/header.view.php
@@ -6,4 +6,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href=" <?= ASSETS ?>css/styles.css">
+    <link rel="stylesheet" href=" <?= ASSETS ?>css/ravien.css">
 </head>
