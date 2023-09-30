b926f646d7dbaa4e44ce71416201b4c6483e7e05
Iflal Ismalebbe <iflalm5@gmail.com>
2023-09-30 12:39:21 +0530

    Admin page created
    with dummy graph. and add iflal.css stuffs. and css use with "if-" .

--- private/views/admin.dashboard.view.php
+++ private/views/admin.dashboard.view.php
@@ -1,4 +1,3 @@
-
 <?php $this->view("./includes/header") ?>
 
 <body>
@@ -10,23 +9,170 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-                        <div class="card card-body">
-                            
+
+                        <div class="if-txt-wrapper">Hello,&nbsp;&nbsp;Admin</div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="if-widgets">
+                            <div class="frame">
+                                <div class="div">
+                                    <div class="frame-2">
+                                        <div class="impressions">Number of Train Onboard</div>
+                                    </div>
+                                    <div class="number">26</div>
+                                </div>
+                            </div>
+                            <div class="frame">
+                                <div class="frame-3">
+                                    <div class="frame-4">
+                                        <div class="impressions">Inquiries</div>
+                                    </div>
+                                    <div class="number">35</div>
+                                </div>
+                            </div>
+                            <div class="frame">
+                                <div class="div">
+                                    <div class="frame-4">
+                                        <div class="impressions">Staffs On Duty</div>
+                                    </div>
+                                    <div class="number">1675</div>
+                                </div>
+                            </div>
+                            <div class="frame">
+                                <div class="div">
+                                    <div class="frame-4">
+                                        <div class="impressions">Cancelled Trains</div>
+                                    </div>
+                                    <div class="number">4</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
+
+
+            <div class="container">
                 <div class="row">
-
-                    <div class="col-6">
-                        <p>Welcome to the dashboard</p>
+                    <div class="col-12">
+                        <div class="if-txt-wrapper2"> System User, Traffic Details</div>
                     </div>
-                    <div class="col-6">
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias voluptas natus, laboriosam veritatis itaque repudiandae, voluptate minima aliquid quod ea cumque, necessitatibus voluptatum quidem assumenda tempore blanditiis et quia earum.</p>
-                    </div>
-
                 </div>
             </div>
-        </main>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="if-box">
+                            <div class="group">
+                                <div class="group-wrapper">
+                                    <div class="box">
+                                        <div class="group-wrapper">
+                                            <div class="box">
+                                                <div class="frame">
+                                                    <div class="line-chart">
+                                                        <div class="overlap-group">
+                                                            <div class="plot-area">
+                                                                <div class="y-axis-containner">
+                                                                    <div class="container-wrapper">
+                                                                        <div class="container">
+                                                                            <div class="line-container"><img
+                                                                                    class="line" src="img/image.svg" />
+                                                                            </div>
+                                                                            <div class="y-axis-container">
+                                                                                <img class="left-divider"
+                                                                                    src="img/left-divider.svg" />
+                                                                                <div class="value">
+                                                                                    <div class="text">20</div>
+                                                                                </div>
+                                                                                <img class="fixed-right-divider"
+                                                                                    src="img/fixed-right-divider-2.svg" />
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="container-wrapper">
+                                                                        <div class="container">
+                                                                            <div class="line-container"><img
+                                                                                    class="line" src="img/line.svg" />
+                                                                            </div>
+                                                                            <div class="y-axis-container">
+                                                                                <img class="left-divider"
+                                                                                    src="img/left-divider-2.svg" />
+                                                                                <div class="value">
+                                                                                    <div class="text">10</div>
+                                                                                </div>
+                                                                                <img class="fixed-right-divider"
+                                                                                    src="img/fixed-right-divider.svg" />
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="container-wrapper">
+                                                                        <div class="container">
+                                                                            <div class="line-container"><img
+                                                                                    class="line" src="img/line-2.svg" />
+                                                                            </div>
+                                                                            <div class="y-axis-container">
+                                                                                <img class="left-divider"
+                                                                                    src="img/left-divider-3.svg" />
+                                                                                <div class="value">
+                                                                                    <div class="text">0</div>
+                                                                                </div>
+                                                                                <img class="fixed-right-divider"
+                                                                                    src="img/fixed-right-divider-3.svg" />
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="container-wrapper">
+                                                                    <div class="container">
+                                                                        <div class="x-axis-area">
+                                                                            <div class="x-axis-value">
+                                                                                <div class="x-axis">
+                                                                                    <div class="text-wrapper">
+                                                                                        <div class="div">Mon</div>
+                                                                                    </div>
+                                                                                </div>
+                                                                                <div class="x-axis">
+                                                                                    <div class="text-wrapper">
+                                                                                        <div class="text-2">Tue</div>
+                                                                                    </div>
+                                                                                </div>
+                                                                                <div class="value-wrapper">
+                                                                                    <div class="text-wrapper">
+                                                                                        <div class="text-3">Wed</div>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="mark-area">
+                                                                <div class="mark-container-wrapper">
+                                                                    <img class="mark-container"
+                                                                        src="img/mark-container.svg" />
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    </main>
     </div>
 
 
