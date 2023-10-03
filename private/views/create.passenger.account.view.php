0958d4cac8912b401f8a5e8c3e3c712be79a2ca8
achchudhan <123426212+achchudhan@users.noreply.github.com>
2023-10-03 23:11:23 +0530

    create passenger account

--- private/views/create.passenger.account.view.php
+++ private/views/create.passenger.account.view.php
@@ -6,104 +6,149 @@
         <main>
             <div class="container">
                 <div class="row">
-                    <div class="col-12">
-                        <div class="ach-box">
-                            <div class="group">
-                                <div class="overlap">
-                                    <div class="frame">
-                                        <div class="div">
-                                            <div class="group-2">
-                                                <img class="tracknbook-logo"
-                                                    src="<?= ASSETS ?>images/track-n-book-logo-1.png" />
-                                                <div class="overlap-group">
-                                                    <img class="img"
-                                                        src="<?= ASSETS ?>images/track-n-book-logo-1.png" />
-                                                    <div class="text-wrapper">Register</div>
-                                                </div>
-                                            </div>
-                                            <div class="overlap-2">
-                                                <div class="text-wrapper-2">First Name</div>
-                                                <div class="text-wrapper-3">Your email ID</div>
-                                                <div class="text-wrapper-4">Your Phone No</div>
-                                                <div class="text-wrapper-5">Password</div>
-                                                <div class="text-wrapper-6">Confirm Password</div>
-                                                <div class="text-wrapper-7">Last Name</div>
-                                                <div class="input-field">
-                                                    <div class="text">
-                                                        <div class="text-wrapper-8">************</div>
-                                                        <img class="icon" src="img/icon-3.svg" />
-                                                    </div>
-                                                </div>
-                                                <div class="text-wrapper-9">NIC No</div>
-                                                <div class="div-wrapper">
-                                                    <div class="text">
-                                                        <input class="achchuthan-gmail-com"
-                                                            placeholder="achchuthan@gmail.com" type="email" />
-                                                        <img class="icon-2" src="img/icon-2.svg" />
-                                                    </div>
-                                                </div>
-                                                <div class="input-field-2">
-                                                    <div class="text">
-                                                        <div class="text-wrapper-8">Ex : 0776712345</div>
-                                                        <img class="icon-3" src="img/icon-4.svg" />
-                                                    </div>
-                                                </div>
-                                                <div class="input-field-3">
-                                                    <div class="text">
-                                                        <div class="text-wrapper-8">************</div>
-                                                        <img class="icon-4" src="img/image.svg" />
-                                                    </div>
-                                                </div>
-                                                <div class="input-field-4">
-                                                    <div class="text">
-                                                        <div class="text-wrapper-8">Ex : Achchuthan</div>
-                                                        <img class="icon-5" src="img/icon.svg" />
-                                                    </div>
-                                                </div>
-                                                <div class="input-field-5">
-                                                    <div class="text">
-                                                        <div class="text-wrapper-8">Ex : Kalanithy</div>
-                                                        <img class="icon-6" src="img/icon-8.svg" />
-                                                    </div>
-                                                </div>
-                                                <div class="input-field-6">
-                                                    <div class="text">
-                                                        <div class="text-wrapper-8">Ex : 992090854V</div>
-                                                        <img class="icon-7" src="img/icon-7.svg" />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <p class="p">Re - send activation mail?</p>
-                                            <div class="input-field-7">
-                                                <div class="text-2">
-                                                    <div class="text-wrapper-10">Mr</div>
-                                                    <img class="icon-8" src="img/icon-5.svg" />
-                                                </div>
-                                            </div>
-                                            <div class="input-field-8">
-                                                <div class="text-2">
-                                                    <div class="text-wrapper-10">NIC</div>
-                                                    <img class="icon-8" src="img/icon-6.svg" />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <button class="buttons">
-                                        <div class="go-wrapper">
-                                            <div class="go">Register</div>
-                                        </div>
-                                    </button>
-                                </div>
+                    <div class="col-2">
+                        <div class="field">
+                            
+                            <div>
+                                <select name="" id="" value="Mr." class="select-field">
+                                    <option value="">Mr.</option>
+                                    <option value="">Mrs.</option>
+                                    <option value="">Miss.</option>
+                                </select>
                             </div>
                         </div>
+                        <div class="field-select">
 
+                        </div>
+                        <div class="field">
+                        
+                            <div>
+                                <select name="" id="" value="Mr." class="select-field">
+                                    <option value="">NIC</option>
+                                    <option value="">Passport</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-5">
+                        <div class="field">
+                            <label>First Name</label>
+                            <input placeholder="Ex : Achchuthan" class="text-field" type="text" />
+                        </div>
+                        <div class="field">
+                            <label>Last Name</label>
+                            <input placeholder="Ex : Kalanithy" class="text-field" type="text" />
+                        </div>
+                        <div class="field">
+                            <label>NIC No</label>
+                            <input placeholder="Ex : 992090854V" class="text-field" type="text" />
+                        </div>
+                        <div class="field">
+                            <label>Your email ID</label>
+                            <input placeholder="Ex : achchuthan@gmail.com" class="text-field" type="text" />
+                        </div>
+                        <div class="field">
+                            <label>Your Phone No</label>
+                            <input placeholder="Ex : 0776712345" class="text-field" type="text" />
+                        </div>
+                        <div class="field">
+                            <label>Password</label>
+                            <input placeholder="Ex : ************" class="text-field" type="text" />
+                        </div>
+                        <div class="field">
+                            <label>Confirm Password</label>
+                            <input placeholder="Ex : ************" class="text-field" type="text" />
+                        </div>
+                        <div class="activation-field">
+                            <a href="/"> <b>Re - send activation mail?</b></a>
+                            <button class="button"> Register</button>
+                        </div>
+                    </div>
+
+                    <div class="col-4 image-field">
+                        <img class="image" src="<?= ASSETS ?>images/Logo on create passenger account page.png">
                     </div>
                 </div>
-            </div>
         </main>
+        <?php $this->view("./includes/footer") ?>
+
     </div>
 
 
 </body>
+
+<style>
+    .image-field {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .field-select {
+        height:112px;
+    }
+
+    .button {
+        display: inline-flex;
+        padding: 16px;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 10px;
+        cursor: pointer;
+
+        border-radius: 8px;
+        border: 1px solid #000;
+        background: #FFF;
+    }
+
+    .button:hover {
+        background-color: lightgray;
+        color: blue;
+    }
+
+    .activation-field {
+        display: flex;
+        justify-content: space-between;
+
+    }
+
+    .activation-field a {
+        text-decoration-line: underline;
+    }
+
+    .field {
+        margin-bottom: 6px;
+    }
+
+    .text-field {
+        display: flex;
+        width: 100%;
+        padding: 16px;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 10px;
+        border-radius: 8px;
+        border: 1px solid #CCC;
+        background: var(--W-Background, #FFF);
+    }
+
+    .select-field {
+        display: flex;
+        width: 135.492px;
+        padding: 16px;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 10px;
+        border-radius: 8px;
+        border: 1px solid #CCC;
+        background: var(--W-Background, #FFF);
+    }
+
+
+    .image {
+        margin-left: 182px;
+        width: 542px;
+    }
+</style>
 
 </html>
