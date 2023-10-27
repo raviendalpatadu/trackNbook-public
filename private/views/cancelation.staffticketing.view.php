31ef7a11ad74e091c8c06bcb7493f7a7330fa0ff
moushikakri <moushika123kriyanjalee@gmail.com>
2023-10-28 04:16:57 +0530

    cancelation...

--- private/views/cancelation.staffticketing.view.php
+++ private/views/cancelation.staffticketing.view.php
@@ -7,11 +7,11 @@
      <?php $this->view("./includes/dashboard-navbar") ?>
             <main>
                 <div class="container d-flex justify-content-center">
-                    <div class="cancel-container justify-align-center">     
-                        <div class="row mb-20 "> 
-                            <div class="col-12 d-flex align-items-start">
+                    <div class="cancel-container justify-align-center mt-50">     
+                        <div class="row mb-30 "> 
+                            <div class="col-12 d-flex align-items-center">
                                 <div class="text-inputs">
-                                    <div class="input-text-label">Passenger ID</div>
+                                    <div class="input-text-label text lightgray-font">Passenger ID</div>
                                         <div class="width-fill">
                                             <select class="dropdown" placeholder="Please choose">
                                                 <option value=""selected>All</option>
@@ -27,10 +27,10 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="row mb-20 "> 
+                        <div class="row mb-30 "> 
                             <div class="col-12 d-flex align-items-start">
                                 <div class="text-inputs">
-                                    <div class="input-text-label">Ticket Number</div>
+                                    <div class="input-text-label text lightgray-font">Ticket Number</div>
                                         <div class="width-fill">
                                             <select class="dropdown" placeholder="Please choose">
                                                 <option value=""selected>All</option>
@@ -46,10 +46,10 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="row mb-20 "> 
+                        <div class="row mb-30 "> 
                             <div class="col-12 d-flex align-items-start">
                                 <div class="text-inputs">
-                                    <div class="input-text-label">Train Number</div>
+                                    <div class="input-text-label text lightgray-font">Train Number</div>
                                         <div class="width-fill">
                                             <select class="dropdown" placeholder="Please choose">
                                                 <option value=""selected>All</option>
@@ -64,21 +64,31 @@
                                     <div class="assistive-text display-none">Assistive Text</div>
                                 </div>
                             </div>
-                        </div>
-                        
+                        </div>       
                     </div>
                     <div class="row">
-                        <div class="col-12 d-flex align-items-center flex-column">
-                            <button class="button"><a href="<?=ROOT?>passenger/details">
-                                <div class="button-base">
-                                    <div class="text">Pay</div>
+                        <div class="col-12 line"></div>
+                    </div>
+                    <div class="row mb-20">
+                        <div class="col-12">
+                        <div class="d-flex justify-content-end align-items-end g-20 width-fill">
+                                    <button class="button">
+                                        <div class="button-base">
+                                            <div class="text">Cancel</div>
+                                        </div>
+                                    </button>
+                                    <button class="button"><a href="<?=ROOT?>passenger/details">
+                                        <div class="button-base">
+
+                                            <div class="text">Proceed</div>
                                             <svg class="arrow-right" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M4.16675 9.99935H15.8334M15.8334 9.99935L10.0001 4.16602M15.8334 9.99935L10.0001 15.8327" stroke="#344054" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round" />
                                             </svg>
-                                </div></a>
-                            </button>
+                                        </div></a>
+                                    </button>
+                                </div>
                         </div>
-                    </div>                     
+                                  
                 </div>
             </main>
         <?php $this->view('includes/footer'); ?>
