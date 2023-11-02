d69a63d56ebc89e833dd89e892b7a25c815ed017
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2023-11-02 19:15:58 +0530

    to make user update for train and user

--- private/controllers/Admin.php
+++ private/controllers/Admin.php
@@ -14,25 +14,36 @@ class Admin extends Controller
 
     function getUsers(){
 
-        $resevation = new Reservations();
+        $user = new Users();
 
-        $train = new Trains();
+        // $train = new Trains();
 
         $data = array();
 
-        $data['trains'] = $train->findAll();
-        $data['reservations'] = $resevation->getReservation();
-        if (isset($_POST['submit']) && !empty($_POST['reservation_date'])) {
-            $data['reservations'] = $resevation->getReservations('reservation_date', $_POST['reservation_date']);
-        }
-        if (isset($_POST['submit']) && !empty($_POST['reservation_passenger_nic'])) {
-            $data['reservations'] = $resevation->getReservations('reservation_passenger_nic', $_POST['reservation_passenger_nic']);
-        }
-        if (isset($_POST['submit']) && !empty($_POST['reservation_train_id'])) {
-            $data['reservations'] = $resevation->getReservations('reservation_train_id', $_POST['reservation_train_id']);
-        }
+        // $data['trains'] = $train->findAll();
+        $data['users'] = $user->findAll();
+        // if (isset($_POST['submit']) && !empty($_POST['reservation_date'])) {
+        //     $data['users'] = $user->getusers('reservation_date', $_POST['reservation_date']);
+        // }
+        // if (isset($_POST['submit']) && !empty($_POST['reservation_passenger_nic'])) {
+        //     $data['users'] = $user->getusers('reservation_passenger_nic', $_POST['reservation_passenger_nic']);
+        // }
+        // if (isset($_POST['submit']) && !empty($_POST['reservation_train_id'])) {
+        //     $data['users'] = $user->getusers('reservation_train_id', $_POST['reservation_train_id']);
+        // }
 
         $this->view('display.users.admin', $data);
+    }
+
+    function updateUser($id = ''){
+        $user = new Users();
+        $data = array();
+
+        $data['user'] = $user->whereOne('user_id',$id);
+        
+
+
+        $this->view('update.user.admin', $data);
     }
 
     function search($id = '')
