5cf9d2d698ac5df4ebcb97d3ed20f45842e83c1c
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2023-11-02 14:37:16 +0530

    test

--- private/views/home.staffticketing.view.php
+++ private/views/home.staffticketing.view.php
@@ -114,20 +114,20 @@
 
                                     </div>
 
-                                    <div class="d-flex align-items-end justify-content-end flex-fill mt-10">
-                                        <!-- <button class="button"><a href="trains/available"> -->
-                                        <div class="button-base"><a href="<?= ROOT ?>staffticketing/trains">
-                                        <div>
-                                            <input class="text"type="submit" name="submit" value="Search" />
-                                            <svg class="arrow-right" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.16675 9.99935H15.8334M15.8334 9.99935L10.0001 4.16602M15.8334 9.99935L10.0001 15.8327" stroke="#344054" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round" />
-                                            </svg>
-                                            </div>
-                                        </div>
-                                        </a>
-                                        <!-- </button> -->
-                                    </div>
                                 </form>
+                                <div class="d-flex align-items-end justify-content-end flex-fill mt-10">
+                                    <!-- <button class="button"><a href="trains/available"> -->
+                                    <div class="button-base"><a href="<?= ROOT ?>staffticketing/trains">
+                                    <div>
+                                        <input class="text"type="submit" name="submit" value="Search" />
+                                        <svg class="arrow-right" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M4.16675 9.99935H15.8334M15.8334 9.99935L10.0001 4.16602M15.8334 9.99935L10.0001 15.8327" stroke="#344054" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round" />
+                                        </svg>
+                                        </div>
+                                    </div>
+                                    </a>
+                                    <!-- </button> -->
+                                </div>
                             </div>
                         </div>
                     </div>
