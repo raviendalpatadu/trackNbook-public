6c342bb7752908d6214a2153de5fb14087eec984
Iflal <iflalm5@gmail.com>
2024-02-27 13:56:20 +0530

    login whitespace

--- private/controllers/Login.php
+++ private/controllers/Login.php
@@ -26,7 +26,6 @@ class Login extends Controller
                     $this->redirect('home');
                 }
                 
-               
                 
             }
             else{
