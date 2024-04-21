5f3e5e4db57c772f9199aa3c23420025ffa8a0d4
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2024-04-22 04:13:19 +0530

    minnor chnges to add route view

--- private/views/add.route.admin.view.php
+++ private/views/add.route.admin.view.php
@@ -149,84 +149,10 @@ $stationCount = isset($_POST['station_count']) ? intval($_POST['station_count'])
 
                 stationDropdowns.innerHTML = html;
 
-                // makeSelectBox('.dropdown');
                 const outputContainer = $('#stationDropdowns');
+                makeSelectDropdown(outputContainer);
 
-                // bug when compartment ro is genarates the top select tags are not working
-                $(outputContainer).find("select.dropdown").each(function() {
-                    var dropdown = $("<div />").addClass("dropdown selectDropdown");
-
-                    $(this).wrap(dropdown);
-
-                    var label = $("<span />")
-                        .text($(this).attr("placeholder"))
-                        .insertAfter($(this));
-                    var list = $("<ul />");
-
-                    label.attr("class", "input-field");
-
-                    $(this)
-                        .find("option")
-                        .each(function() {
-                            list.append($("<li />").append($("<a />").text($(this).text())));
-                        });
-
-                    list.insertAfter($(this));
-
-                    if ($(this).find("option:selected").length) {
-                        label.text($(this).find("option:selected").text());
-                        list
-                            .find("li:contains(" + $(this).find("option:selected").text() + ")")
-                            .addClass("active");
-                        $(this).parent().addClass("filled");
-                    }
-                });
-
-
-                $(outputContainer).on("click touch", ".selectDropdown ul li a", function(e) {
-                    e.stopImmediatePropagation();
-                    console.log($(this).text());
-                    var dropdown = $(this).parent().parent().parent();
-                    // console.log(dropdown);
-                    var active = $(this).parent().hasClass("active");
-                    var label = active ?
-                        dropdown.find("select").attr("placeholder") :
-                        $(this).text();
-
-
-                    console.log($(this));
-
-
-                    dropdown.find("option").prop("selected", false);
-                    dropdown.find("ul li").removeClass("active");
-
-                    dropdown.toggleClass("filled", !active);
-                    dropdown.children("span").text(label);
-                    if (!active) {
-                        dropdown
-                            .find("option:contains(" + $(this).text() + ")")
-                            .prop("selected", true);
-
-                        $(this).parent().addClass("active");
-                    }
-
-                    dropdown.removeClass("open");
-
-                    //trigger change event
-                    dropdown.find("select").trigger("change");
-                });
-
-                $(".dropdown > span").on("click touch", function(e) {
-                    var self = $(this).parent();
-                    self.toggleClass("open");
-                });
-
-                $(outputContainer).on("click touch", function(e) {
-                    var dropdown = $(".dropdown");
-                    if (dropdown !== e.target && !dropdown.has(e.target).length) {
-                        dropdown.removeClass("open");
-                    }
-                });
+                
 
             });
         });
