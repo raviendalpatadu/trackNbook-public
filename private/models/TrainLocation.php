bd3342bc69926c38e265652ca24203a82612d8c2
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2024-04-21 14:19:57 +0530

    train location model columns chaged

--- private/models/TrainLocation.php
+++ private/models/TrainLocation.php
@@ -3,15 +3,15 @@
 class TrainLocation extends Model
 {
     protected $table = 'tbl_train_location';
-    protected $allowedColumns = ['train_id', 'station_id', 'train_stop_time', 'stop_no'];
+    protected $allowedColumns = ['train_id', 'date', 'train_location', 'train_location_updated_time'];
    
 
-    public function updateStopStation($station_id)
-    {
-        $query = "UPDATE tbl_train_location tl
-        JOIN $this->table tss ON tbl.train_location = tss.station_id
-        SET tl.station_id = :station_id
-        WHERE tss.train_id = :train_id";
-        return $this->query($query, ['station_id' => $station_id]);
-    }
+    // public function updateStation($station_id)
+    // {
+    //     $query = "UPDATE tbl_train_location tl
+    //     JOIN $this->table tss ON tbl.train_location = tss.station_id
+    //     SET tl.station_id = :station_id
+    //     WHERE tss.train_id = :train_id";
+    //     return $this->query($query, ['station_id' => $station_id]);
+    // }
 }
