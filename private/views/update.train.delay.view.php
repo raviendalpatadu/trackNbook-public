3ae3532d2efcd30cfa21c8e14a06ab427a374e4d
achchudhan <123426212+achchudhan@users.noreply.github.com>
2024-04-28 19:36:41 +0530

    push for sm dashboard card work

--- private/views/update.train.delay.view.php
+++ private/views/update.train.delay.view.php
@@ -23,29 +23,30 @@ if (isset($data['trains']) && $data['trains'] != 0) {
                         <div class="delay-container-box mt-30">
                             <div class="card-update-delay ">
                                 <div class="row mb-20 ">
-                                    <div class="col-12 d-flex align-items-start flex-column line">
+                                    <div class="col-12 d-flex align-items-start flex-column line mb-10">
                                         <h1>Update Train Delay</h1>
                                     </div>
                                     <form method="POST" action="<?= ROOT ?>/traindriver/traindelay">
                                         <div class="text-inputs mb-10">
-                                            <div class="input-text-label">Train Name:
+                                            <div class="ad-width-100">Train Name:
                                                 <?php echo (array_key_exists('train', $data)) ? ucfirst($data['train']->train_name) : ''; ?>
                                             </div>
                                         </div>
-                                        <div class="input-text-label text lightgray-font mb-10">
-                                            <label for="station">Current Station: 
+                                        <div class="text-inputs mb-10">
+                                            <label for="station">Current Station:
                                                 <?php echo (array_key_exists('location', $data)) ? ucfirst($data['location']->station_name) : ''; ?></label>
                                         </div>
 
-                                        <div class="text-inputs mb-10 ">
-                                            <div class="input-text-label">Reason</div>
-                                            <div class="input-field">
+                                        <div class="text-inputs mb-10">
+                                            <div class="ad-width-100">Reason</div>
+                                            <div class="input-field-update-delay">
                                                 <div class="text">
-                                                    <input type="text" name="reason" class="type-here"
-                                                        placeholder="Type here">
+                                                    <textarea name="reason" class="type-here" placeholder="Type here"
+                                                        rows="5"></textarea>
                                                 </div>
                                             </div>
                                         </div>
+
 
                                         <div class="row d-flex delay-g-8 justify-content-center">
                                             <div class="col-4">
@@ -55,7 +56,7 @@ if (isset($data['trains']) && $data['trains'] != 0) {
                                                     </div>
                                                 </button>
                                             </div>
-                                            <div class="col-4"> 
+                                            <div class="col-4">
                                                 <button type="button" class="button mt-20" id="reject"
                                                     onclick="clearForm()">
                                                     <div class="button-base bg-Selected-red">
