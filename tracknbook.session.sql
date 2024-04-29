c4d0154cacac81c1522092eec253b014b96d711e
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2024-04-29 11:22:57 +0530

    before merge if final

--- tracknbook.session.sql
+++ tracknbook.session.sql
@@ -1,20 +1,23 @@
-
-WITH current_station AS (
-    SELECT *
-    FROM tbl_train_stop_station
-    WHERE train_id = 26 AND station_id = 1
-),
-
-next_stations AS (
-    SELECT *
-    FROM tbl_train_stop_station
-    WHERE train_id = 26 AND stop_no > (SELECT stop_no FROM current_station)
-)
-
-SELECT *
-FROM next_stations ns
-
-JOIN tbl_reservation r ON  ns.train_id = r.reservation_train_id AND ns.station_id = r.reservation_start_station
-
-
-
+SELECT i.*,
+    r.*,
+    u.*,
+    t.train_start_station,
+    t.train_end_station,
+    t.train_name,
+    t.train_start_time,
+    t.train_end_time,
+    t.train_type,
+    ctt.compartment_class_type,
+    tt.train_type,
+    start_st.station_name AS start_station_name,
+    end_st.station_name AS end_station_name
+FROM tbl_inquiry i
+    INNER JOIN tbl_user u ON i.inquiry_passenger_id = u.user_id
+    INNER JOIN tbl_reservation r ON i.inquiry_ticket_id = r.reservation_ticket_id
+    INNER JOIN tbl_train t ON r.reservation_train_id = t.train_id
+    JOIN tbl_compartment c ON r.reservation_compartment_id = c.compartment_id
+    JOIN tbl_compartment_class_type ctt ON c.compartment_class_type = ctt.compartment_class_type_id
+    JOIN tbl_train_type tt ON t.train_type = tt.train_type_id
+    JOIN tbl_station start_st ON t.train_start_station = start_st.station_id
+    JOIN tbl_station end_st ON t.train_end_station = end_st.station_id
+GROUP BY i.inquiry_id;
